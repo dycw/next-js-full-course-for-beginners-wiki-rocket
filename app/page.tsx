3f802e02745f9529f2b58d5263c5929655d84c30
Derek Wan <d.wan@icloud.com>
2023-06-03 11:29:10 +0800

    Finish lesson 2 (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Welcome to Next.js",
+};
+
 export default function Home() {
-  return <h1>Next.js template</h1>;
+  return (
+    <>
+      <main>
+        <h1>Hello World</h1>
+        <Link href="/about">Go to About Page</Link>
+      </main>
+    </>
+  );
 }
